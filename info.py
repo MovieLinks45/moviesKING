c4967f9e6fb4ef439cf4772452dfbf043bb84480
MovieLinks45 <144549911+MovieLinks45@users.noreply.github.com>
2025-04-11 22:26:49 +0530

    Update info.py

--- info.py
+++ info.py
@@ -13,20 +13,20 @@ def is_enabled(value, default):
 #---------------------------------------------------------------
 #---------------------------------------------------------------         ,
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ.get('API_ID', '21484'))
-API_HASH = environ.get('API_HASH', 'db6286a041adf07ef4244e')
-BOT_TOKEN = environ.get('BOT_TOKEN', '7807I9ygow9sLqNQuI7hsWPm5FLNfBr9c')
+API_ID = int(environ.get('API_ID', '22968232'))
+API_HASH = environ.get('API_HASH', 'c5dcf6a35d7ef8f614bb6609f0467395')
+BOT_TOKEN = environ.get('BOT_TOKEN', '7418608135:AAHsMg-3Mb4ZbDbHwZ0tAm2PWK0wG6CT1JM')
 #---------------------------------------------------------------
 #---------------------------------------------------------------
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5016109398').split()]
-USERNAME = environ.get('USERNAME', "https://t.me/Innocent_babe_dead") # ADMIN USERNAME
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002190681742'))
-MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/+K2kgCBgaat80YWQ9')
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002190681742').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '7049124442').split()]
+USERNAME = environ.get('USERNAME', "https://t.me/movielinkz4U") # ADMIN USERNAME
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002337053920'))
+MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/movielinkz4you')
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002206875745').split()]
 #---------------------------------------------------------------
 #---------------------------------------------------------------
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv:learningbajority&appName=learningbots")
-DATABASE_NAME = environ.get('DATABASE_NAME', "learningbots")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb://Moviesbot:Admin2mango@ac-yssyycu-shard-00-00.oxxnavq.mongodb.net:27017,ac-yssyycu-shard-00-01.oxxnavq.mongodb.net:27017,ac-yssyycu-shard-00-02.oxxnavq.mongodb.net:27017/?replicaSet=atlas-bhb9ko-shard-0&ssl=true&authSource=admin&retryWrites=true&w=majority&appName=TheKing")
+DATABASE_NAME = environ.get('DATABASE_NAME', "Moviesbot")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 #---------------------------------------------------------------
 #---------------------------------------------------------------
